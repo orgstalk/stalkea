d7c30bbf71a8fc5fd029ca8f790e25cecd2ed6e6
leandrocardososiva1010-hash <leandrocardososiva1010@gmail.com>
2026-01-27 01:40:19 -0300

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,19 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+const colorMode = useColorMode()
+colorMode.preference = 'dark'
+
 useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
   script: [
     {
-      children: `
-!function (w, d, t) {
+      children: `!function (w, d, t) {
   w.TiktokAnalyticsObject=t;
   var ttq=w[t]=w[t]||[];
-  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
   ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}; 
   for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
   ttq.load=function(e){
@@ -17,14 +24,25 @@ useHead({
     s.parentNode.insertBefore(n,s)
   };
   ttq.load('D5RTRI3C77UECCBSD1F0');
-  ttq.page();
-}(window, document, 'ttq');
-      `
+}(window, document, 'ttq');`
     }
   ]
 })
-</script>
 
-<template>
-  <NuxtPage />
-</template>
+const router = useRouter()
+
+onMounted(() => {
+  if (window.ttq) {
+    window.ttq.page()
+  }
+})
+
+watch(
+  () => router.currentRoute.value.fullPath,
+  () => {
+    if (window.ttq) {
+      window.ttq.page()
+    }
+  }
+)
+</script>
